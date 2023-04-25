8c57f039e2aa240a1f7a237d51505bf268fdd214
divyanshhaagahelia <91049526+divyanshhaagahelia@users.noreply.github.com>
2023-04-25 16:18:23 +0300

    Delete src directory

--- src/main/java/com/example/CountryFinal/demo/domain/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.CountryFinal.demo.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CountryRepository extends CrudRepository<Country, Long> {
-	
-}
